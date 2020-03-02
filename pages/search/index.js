9f52d37c92f359fee4d3276ea70387bb268318e1
一打砖头拍不倒 <lyq15360562781@163.com>
2020-03-02 23:55:20 +0800

    引入阿里图标

--- pages/search/index.js
+++ pages/search/index.js
@@ -2,11 +2,18 @@
 import request from '../../utils/request.js'
 Page({
   data : {
-    inputSearchList : { },
-    isShow : true,
+    inputSearchList : { },//搜索数据列表
+    isShow : true, //是否显示搜索历史
+    searchHistoryList : [],//搜索历史列表
+    value : '',//搜素框的值
   },
   inputSerach(e){
     let { value } = e.detail
+    // let arr = []
+    // arr.push(value)
+    // this.setData({
+    //   searchHistoryList: arr
+    // })
     request({
       url: '/api/public/v1/goods/qsearch',
       data : {
@@ -14,11 +21,12 @@ Page({
       }
     }).then((res)=>{
       let { message} = res.data
+      console.log(message)
       this.setData({
-        inputSearchList: {}
+        inputSearchList: message,
+        value 
       })
-      if(message.length > 0){
-        console.log('aaaaaaaaaaa')
+      if(message ){
         this.setData({
           isShow : false
         })
