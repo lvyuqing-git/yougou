77110af7ca1ab9f194d04ea31918834d3edc2994
一打砖头拍不倒 <lyq15360562781@163.com>
2020-03-04 11:18:58 +0800

    '实现商品详情页轮播图样式修整和轮播图点击展示'

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,19 +1,32 @@
 import request from '../../utils/request.js'
 Page({
   data: {
-    commodityInfo : {}
+    commodityInfo : {},
+    urls : []
+  },
+  previewImage(e){
+    let { index } = e.currentTarget.dataset
+    
+    wx.previewImage({
+      current: this.data.urls[index], // 当前显示图片的http链接
+      urls: this.data.urls // 需要预览的图片http链接列表
+    })
   },
   onLoad: function (options) {
     request({
       url: '/api/public/v1/goods/detail',
       data : {
-        // goods_id: options.goods_id
-        goods_id: 43983
+        goods_id: options.goods_id
       }
     }).then((res)=>{
       console.log(res)
+      const { message } = res.data
+      let arr = message.pics.map((item)=>{
+        return item.pics_big
+      })
       this.setData({
-        commodityInfo: res.data.message
+        commodityInfo: message,
+        urls: arr
       })
     })
   }
