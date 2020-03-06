f8b003a8a01e563599cad9746f4005d7dd77a01c
一打砖头拍不倒 <lyq15360562781@163.com>
2020-03-06 18:51:17 +0800

    '获取收货地址'

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,13 +1,36 @@
 // pages/cart/index.js
 Page({
-
-
   data: {
     carData : false,
+    addressInfo : {}
   },
 
   onLoad: function (options) {
-
+    wx.getStorage({
+      key: 'address',
+      success:(res)=> {
+        this.setData({
+          addressInfo: res.data
+        })
+      }
+    })
   },
-
+  getAddress(){
+    wx.chooseAddress({
+      success : (res)=> {
+        let obj = {
+          userName: res.userName,
+          address: res.provinceName + res.cityName + res.countyName + res.detailInfo ,
+          telNumber: res.telNumber
+        }
+        this.setData({
+          addressInfo: obj
+        })
+        wx.setStorage({
+          key: "address",
+          data: this.data.addressInfo
+        })
+      }
+    })
+  }
 })
